c1c94532e9aadcbed1daf34e7e12df620bc414b1
alex <camward@yandex.ru>
2016-12-24 17:05:57 +0400

    страницы index и about

--- views/my/index.php
+++ views/my/index.php
@@ -1,9 +1,66 @@
-<?php
-/* @var $this yii\web\View */
-?>
-<h1>my/index</h1>
+<!-- Featured Image Slider -->
+<div id="featured" class="clearfix grid_12">
+    <ul>
+        <li>
+            <a href="portfolio_single.html">
+                <span>Read about this project</span>
+                <img src="images/600x300.gif" alt="" />
+            </a>
+        </li>
+        <li>
+            <a href="portfolio_single.html">
+                <span>Read about this project</span>
+                <img src="images/600x300.gif" alt="" />
+            </a>
+        </li>
+        <li>
+            <a href="portfolio_single.html">
+                <span>Read about this project</span>
+                <img src="images/600x300.gif" alt="" />
+            </a>
+        </li>
+        <li>
+            <a href="portfolio_single.html">
+                <span>Read about this project</span>
+                <img src="images/600x300.gif" alt="" />
+            </a>
+        </li>
+        <li>
+            <a href="portfolio_single.html">
+                <span>Read about this project</span>
+                <img src="images/600x300.gif" alt="" />
+            </a>
+        </li>
+    </ul>
+</div>
+<div class="hr grid_12 clearfix">&nbsp;</div>
 
-<p>
-    You may change the content of this page by modifying
-    the file <code><?= __FILE__; ?></code>.
-</p>
+<!-- Caption Line -->
+<h2 class="grid_12 caption clearfix">Welcome! This is <span>Aurelius</span>, a slick, professional <u>Business</u> &amp; <u>Portfolio</u> theme built to engage the user in your work.</h2>
+
+<div class="hr grid_12 clearfix quicknavhr">&nbsp;</div>
+<div id="quicknav" class="grid_12">
+    <a class="quicknavgrid_3 quicknav alpha" href="portfolio.html">
+        <h4 class="title ">Recent Work</h4>
+        <p>Cras vestibulum lorem et dui mollis sed posuere leo semper. </p>
+        <p style="text-align:center;"><img alt="" src="images/Art_Artdesigner.lv.png" /></p>
+
+    </a>
+    <a class="quicknavgrid_3 quicknav" href="about.html">
+        <h4 class="title ">Learn about us</h4>
+        <p>Cras vestibulum lorem et dui mollis sed posuere leo semper. </p>
+        <p style="text-align:center;"><img alt="" src="images/info.png" /></p>
+
+    </a>
+    <a class="quicknavgrid_3 quicknav" href="blog.html">
+        <h4 class="title ">Read our blog</h4>
+        <p>Cras vestibulum lorem et dui mollis sed posuere leo semper. </p>
+        <p style="text-align:center;"><img alt="" src="images/Blog_Artdesigner.lv.png" /></p>
+
+    </a>
+    <a class="quicknavgrid_3 quicknav" href="#">
+        <h4 class="title ">Follow on Twitter</h4>
+        <p>Cras vestibulum lorem et dui mollis sed posuere leo semper. </p>
+        <p style="text-align:center;"><img alt="" src="images/hungry_bird.png" /></p>
+    </a>
+</div>
